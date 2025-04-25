fad2c431496e178cb85a4ef454c3185d0cc7a2a5
AidanSchiefer <aidanschiefer@gmail.com>
2025-04-25 14:41:12 -0600

    Documentation and slight changes for server.c

--- server.c
+++ server.c
@@ -20,7 +20,7 @@ void terminate(int sig) {
 
 int main() {
 	int server;
-	int target;
+	int target; // represents the client
 	int dummyfd;
 	struct message req;
 	signal(SIGPIPE,SIG_IGN);
@@ -35,16 +35,22 @@ int main() {
 		if (read(server, &req, sizeof(struct message) != sizeof(struct message))){
 			continue;
 		}
+		// ----- Create a response struct -----
 		struct message resp;
 		printf("Received a request from %s to send the message %s to %s.\n",req.source,req.msg,req.target);
 
 		// TODO:
 		// open target FIFO and write the whole message struct to the target FIFO
 		// close target FIFO after writing the message
-
+		// ----- Open the target (client) FIFO -----
 		target = open(req.target, O_WRONLY);
+		// ----- Copy over all attributes from the request -----
 		strcpy(resp.msg, req.msg);
+		strcpy(resp.target, req.target);
+		strcpy(resp.source, req.source);
+		// ----- Write the intended message to the target (client) FIFO -----
 		write(target, &resp, sizeof(struct message));
+		// ----- Clost the target (client) FIFO -----
 		close(target);
 	}
 	close(server);
